b386d5ccdbb032ef69bb987d70d22beea4851907
yh-nr <127526695+yh-nr@users.noreply.github.com>
2023-08-11 12:04:04 +0900

    config_manager.py の更新

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,25 +1,33 @@
 import json
 
-def load_config_from_file(filename):
+SETTINGS_FILE = r'config.json'
+DEFAULT_SETTINGS_FILE = r'./assets/config.json'
+
+def load_config_from_file(filename=SETTINGS_FILE):
     with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+        settings = json.load(f)
+    return settings
+
 
 def save_config_to_file(filename, data):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
+
 def update_setting(btn, num, name):
+    global settings
     settings[btn]['num'] = num
     settings[btn]['name'] = name
-    save_config_to_file('config.json', settings)
+    save_config_to_file(SETTINGS_FILE, settings)
 
-    
-# 設定ファイルを読み込む
-try:
-    SETTINGS_FILE = r'config.json'
-    settings = load_config_from_file(SETTINGS_FILE)
-except:
-    DEFAULT_SETTINGS_FILE = r'./assets/config.json'
-    save_config_to_file('config.json', load_config_from_file(DEFAULT_SETTINGS_FILE))
-    settings = load_config_from_file(SETTINGS_FILE)
-    print('config.jsonを作成したよ！')
+def initialize_settings():
+    global settings
+    try:
+        settings = load_config_from_file()
+    except:
+        save_config_to_file(SETTINGS_FILE, load_config_from_file(DEFAULT_SETTINGS_FILE))
+        settings = load_config_from_file()
+        print('config.jsonを作成したよ！')
+
+# 最初に設定を初期化する
+initialize_settings()
